4b7930801b8062cfb2d3cbd4ef0e83719f48417e
hisman1982 <carlosgreyes82@gmail.com>
2018-08-25 21:39:07 +0200

    feat: animation include in toggle items

--- src/app/shared/toggle-card/toggle-card.component.ts
+++ src/app/shared/toggle-card/toggle-card.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 
 @Component({
   selector: 'app-toggle-card',
   templateUrl: './toggle-card.component.html',
-  styleUrls: ['./toggle-card.component.sass']
+  styleUrls: ['./toggle-card.component.sass'],
+  animations: [
+    trigger('dropDown', [
+      transition(':enter', [
+        style({ height: 0, opacity: 0 }),
+        animate('500ms ease-in', style({ height: '*', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-out', style({ height: 0, opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class ToggleCardComponent implements OnInit {
 
--- src/app/shared/toggle-item-card/toggle-item-card.component.ts
+++ src/app/shared/toggle-item-card/toggle-item-card.component.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-toggle-item-card',
   templateUrl: './toggle-item-card.component.html',
-  styleUrls: ['./toggle-item-card.component.sass']
+  styleUrls: ['./toggle-item-card.component.sass'],
+  animations: [
+    trigger('dropDown', [
+      transition(':enter', [
+        style({ height: 0, opacity: 0 }),
+        animate('500ms ease-in', style({ height: '*', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-out', style({ height: 0, opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class ToggleItemCardComponent implements OnInit {
 
